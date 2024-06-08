6d06c65586660bf0122b0686cdf0f78100fb0f6d
Hein Thant <heinthant2704@gmail.com>
2024-06-08 10:39:05 +0700

    Initial Commit

---
+++ Class1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class1
+//
+//  Created by Hein Thant on 8/6/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
